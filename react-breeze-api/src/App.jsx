f1eb4fd80a1bb725d714fe2af65e619f49c7a435
rashad <akyrashad@gmail.com>
2023-04-24 09:28:28 +0530

    forget and email sending

--- react-breeze-api/src/App.jsx
+++ react-breeze-api/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
 
 import AuthLayout from "./layouts/AuthLayout";
 import GuestLayout from "./layouts/GuestLayout";
@@ -17,6 +18,8 @@ function App() {
                     <Route element={<GuestLayout />}>
                         <Route path="/login" element={<Login />} />
                         <Route path="/register" element={<Register />} />
+                        <Route path="/forgot-password" element={<ForgotPassword />}
+                        />
                     </Route>
                 </Routes>
             </div>
--- react-breeze-api/src/context/AuthContext.jsx
+++ react-breeze-api/src/context/AuthContext.jsx
@@ -59,7 +59,7 @@ export const AuthProvider = ({ children }) => {
 
     return (
         <AuthContext.Provider
-            value={{ user, errors, getUser, login, register, logout }}
+            value={{ user, errors, getUser, login, register, logout , csrf }}
         >
             {children}
         </AuthContext.Provider>
